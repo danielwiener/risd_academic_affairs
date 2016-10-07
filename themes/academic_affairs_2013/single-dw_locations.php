89e1fbfeee9e0e657b6fbd49df8601fc3299f41b
Daniel Wiener <daniel@danielwiener.com>
2016-10-07 17:33:12 -0400

    beginning to make the icon list for single pages
    not sure if it makes sense

--- themes/academic_affairs_2013/single-dw_locations.php
+++ themes/academic_affairs_2013/single-dw_locations.php
@@ -12,15 +12,21 @@ get_header(); ?>
 
   <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
     <header>
-      <h2><?php the_title(); ?></a></h2>
-    </header>
+	<h3>Makers' Assets</h3>
+      <h2><?php the_title(); ?></a>
+<?php $material3_terms = get_the_terms( $post->ID, 'dw_materials' );  
+	foreach ($material3_terms as $material3_term) : ?>
+		<a href="/materials/<?php echo $material3_term->slug; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/icons/<?php echo $material3_term->slug; ?>.png" width="50px" height="50px" class="alignright" ></a>
+	<?php endforeach; ?>
+</h2>
+    </header><br /><br /><br />
     <div><?php the_post_thumbnail('medium'); ?>
 <?php the_content(); ?></div>
     <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
    
     <footer>
       <?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
-       <div><?php echo get_the_term_list( $post->ID, 'dw_materials', 'Materials: ', ', ' );  ?></div>
+       <div><?php echo get_the_term_list( $post->ID, 'dw_materials', 'Makers\' Assets: ', ', ' );  ?></div>
     </footer>
     <nav>
 		<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
