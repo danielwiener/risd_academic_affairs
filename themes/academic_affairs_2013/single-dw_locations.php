e5aa67f8a3c5c148b6b51aecee0ee8c00265a234
Daniel Wiener <daniel@danielwiener.com>
2016-10-07 18:03:44 -0400

    moved icons to the top of the page

--- themes/academic_affairs_2013/single-dw_locations.php
+++ themes/academic_affairs_2013/single-dw_locations.php
@@ -15,7 +15,13 @@ get_header(); ?>
 	<h3>Makers' Assets</h3>
       <h2><?php the_title(); ?></a>
 </h2>
-    </header><br /><br /><br />
+<?php $material3_terms = get_the_terms( $post->ID, 'dw_materials' );  
+	foreach ($material3_terms as $material3_term) : ?>
+	<div class="materials-for-single-locations">
+		<a href="/materials/<?php echo $material3_term->slug; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/icons/<?php echo $material3_term->slug; ?>.png" width="65px" height="65px" class="alignnone" ><br /><?php echo $material3_term->name; ?></a></div>
+	<?php endforeach; ?>
+	<br clear="all" /><hr />
+    </header>
     <div><?php the_post_thumbnail('medium'); ?>
 <?php the_content(); ?></div>
     <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
@@ -24,13 +30,9 @@ get_header(); ?>
       <?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
        <!-- <div> --> <?php // echo get_the_term_list( $post->ID, 'dw_materials', 'Makers\' Assets: ', ', ' ); //solution 1, just words ?>  <!--</div> -->
 
-	<?php $material3_terms = get_the_terms( $post->ID, 'dw_materials' );  
-		foreach ($material3_terms as $material3_term) : ?>
-		<div class="materials-for-single-locations">
-			<a href="/materials/<?php echo $material3_term->slug; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/icons/<?php echo $material3_term->slug; ?>.png" width="55px" height="55px" class="alignnone" ><br /><?php echo $material3_term->name; ?></a></div>
-		<?php endforeach; ?>
+	
 		
-    </footer><br clear="all" />
+    </footer>
     <nav>
 		<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
 		<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
