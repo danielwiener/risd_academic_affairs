58a4f96fa68ebb811ce60810ef718f0a35f13386
Daniel Wiener <daniel@danielwiener.com>
2016-10-08 12:05:38 -0400

    styling for listings, change title

--- themes/academic_affairs_2013/single-dw_locations.php
+++ themes/academic_affairs_2013/single-dw_locations.php
@@ -10,17 +10,16 @@ get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-  <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+  <article <?php post_class('single_locations') ?> id="post-<?php the_ID(); ?>">
     <header>
-	<h3>Makers' Assets</h3>
-      <h2><?php the_title(); ?></a>
+      <h2>Markers' Resources at <?php the_title(); ?></a>
 </h2>
 <?php $material3_terms = get_the_terms( $post->ID, 'dw_materials' );  
 	foreach ($material3_terms as $material3_term) : ?>
 	<div class="materials-for-single-locations">
 		<a href="/materials/<?php echo $material3_term->slug; ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/icons/<?php echo $material3_term->slug; ?>.png" width="65px" height="65px" class="alignnone" ><br /><?php echo $material3_term->name; ?></a></div>
 	<?php endforeach; ?>
-	<br clear="all" /><hr />
+	<br clear="all" />
     </header>
     <div><?php the_post_thumbnail('medium'); ?>
 <?php the_content(); ?></div>
@@ -37,9 +36,6 @@ get_header(); ?>
 		<span class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentytwelve' ) . '</span> %title' ); ?></span>
 		<span class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentytwelve' ) . '</span>' ); ?></span>
 	</nav><!-- .nav-single -->
-
-    <?php comments_template(); ?>
-
   </article>
 
 <?php endwhile; else: ?>
