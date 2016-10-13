9964191a1c233cc8f1ac41a3207c591f13b01ab3
Daniel Wiener <daniel@danielwiener.com>
2016-10-13 12:19:52 -0400

    image map links

--- themes/academic_affairs_2013/page-locations_map.php
+++ themes/academic_affairs_2013/page-locations_map.php
@@ -19,28 +19,28 @@ get_header(); ?>
       <header>
       </header>
       <map name="maploc" id="maploc">
-			<area shape="poly" coords="416,535, 429,522, 436,536, 442,535, 447,547, 432,556" href="Building_161SMain.html" alt="161 South Main"  >
-			<area shape="poly" coords="342,87, 364,83, 373,103, 362,105, 359,96, 346,100" href="Building_187Garage.html" alt="187 Garage"  >
-			<area shape="poly" coords="178, 86, 202, 82, 208, 98, 183, 105" href="Building_189Canal.html" alt="189 Canal St."  >
-			<area shape="poly" coords="324, 266, 352, 255, 360, 268, 330, 279" href="Building_Bank.html" alt="Bank Building"  >
-			<area shape="poly" coords="451, 583, 480, 566, 491, 580, 462, 602" href="Building_BEB.html" alt="BEB"  >
-			<area shape="poly" coords="396, 240, 413, 235, 417, 247, 397, 252" href="Building_Benson.html" alt="Benson Hall"  >
-			<area shape="poly" coords="87, 595, 82, 604, 53, 584, 61, 575" href="Building_CIT.html" alt="CIT/Mason"  >
-			<area shape="poly" coords="334, 283, 390, 262, 395, 277, 343, 298" href="Building_College.html" alt="College Building"  >
-			<area shape="poly" coords="292, 236, 301, 255, 268, 271, 256, 251" href="Building_Design.html" alt="Design Center"  >
-			<area shape="poly" coords="49, 582, 83, 606, 71, 615, 43, 592" href="Building_Fletcher.html" alt="Fletcher Building"  >
-			<area shape="poly" coords="360, 254, 382, 245, 387, 261, 364, 267" href="Building_Memorial.html" alt="Memorial Hall"  >
-			<area shape="poly" coords="301, 212, 315, 205, 334, 255, 318, 261" href="Building_Metcalf.html" alt="Metcalf Building"  >
-			<area shape="poly" coords="217, 180, 260, 163, 272, 191, 230, 211" href="Building_ProvWash.html" alt="Prov Wash"  >
-           <area shape="poly" coords="393, 175, 400, 167, 420, 172, 432, 182, 435, 197, 415, 185" href="Building_South.html" alt="South Hall"  >
-			<area shape="poly" coords="318, 195, 336, 188, 340, 207, 324, 212" href="Building_Waterman.html" alt="Waterman Building"  >
+			<area shape="poly" coords="416,535, 429,522, 436,536, 442,535, 447,547, 432,556" href="/locations/161-s-main-st/" alt="161 South Main"  >
+			<area shape="poly" coords="342,87, 364,83, 373,103, 362,105, 359,96, 346,100" href="/locations/187-garage/" alt="187 Garage"  >
+			<area shape="poly" coords="178, 86, 202, 82, 208, 98, 183, 105" href="/locations/189-canal-street/" alt="189 Canal St."  >
+			<area shape="poly" coords="324, 266, 352, 255, 360, 268, 330, 279" href="/locations/bank-building/" alt="Bank Building"  >
+			<area shape="poly" coords="451, 583, 480, 566, 491, 580, 462, 602" href="/locations/beb-bayard-ewing-building/" alt="BEB"  >
+			<area shape="poly" coords="396, 240, 413, 235, 417, 247, 397, 252" href="/locations/benson-hall/" alt="Benson Hall"  >
+			<area shape="poly" coords="87, 595, 82, 604, 53, 584, 61, 575" href="/locations/cit-mason-building/" alt="CIT/Mason"  >
+			<area shape="poly" coords="334, 283, 390, 262, 395, 277, 343, 298" href="/locations/college-building/" alt="College Building"  >
+			<area shape="poly" coords="292, 236, 301, 255, 268, 271, 256, 251" href="/locations/design-center/" alt="Design Center"  >
+			<area shape="poly" coords="49, 582, 83, 606, 71, 615, 43, 592" href="/locations/fletcher-building/" alt="Fletcher Building"  >
+			<area shape="poly" coords="360, 254, 382, 245, 387, 261, 364, 267" href="/locations/memorial-hall/" alt="Memorial Hall"  >
+			<area shape="poly" coords="301, 212, 315, 205, 334, 255, 318, 261" href="/locations/metcalf-building/" alt="Metcalf Building"  >
+			<area shape="poly" coords="217, 180, 260, 163, 272, 191, 230, 211" href="/locations/prov-wash/" alt="Prov Wash"  >
+           <area shape="poly" coords="393, 175, 400, 167, 420, 172, 432, 182, 435, 197, 415, 185" href="/locations/south-hall/" alt="South Hall"  >
+			<area shape="poly" coords="318, 195, 336, 188, 340, 207, 324, 212" href="/locations/waterman-building/" alt="Waterman Building"  >
            <area shape="poly" coords="193, 318, 223, 293, 252, 322, 228, 351" href="/locations/15-west" alt="15 West"  >
-           <area shape="poly" coords="268, 277, 299, 259, 307, 276, 278, 291" href="Building_Auditorium.html" alt="Auditorium"  >
-           <area shape="poly" coords="351, 185, 361, 182, 365, 197, 355, 198" href="Building_Carr.html" alt="Carr House"  >
-           <area shape="poly" coords="297, 307, 311, 294, 320, 301, 305, 314" href="Building_Market.html" alt="Market House"  >
-           <area shape="poly" coords="245, 232, 267, 223, 275, 236, 253, 246" href="Building_ISB.html" alt="Illustration Studies Building"  >
-           <area shape="poly" coords="328, 56, 367, 50, 368, 76, 336, 80" href="Building_41Meeting.html" alt="41 Meeting St."  >
-           <area shape="poly" coords="279, 17, 305, 13, 312, 45, 282, 47" href="Building_WhatCheer.html" alt="What Cheer Studios"  >
+           <area shape="poly" coords="268, 277, 299, 259, 307, 276, 278, 291" href="/locations/auditorium/" alt="Auditorium"  >
+           <area shape="poly" coords="351, 185, 361, 182, 365, 197, 355, 198" href="/locations/carr-house/" alt="Carr House"  >
+           <area shape="poly" coords="297, 307, 311, 294, 320, 301, 305, 314" href="/locations/market-house/" alt="Market House"  >
+           <area shape="poly" coords="245, 232, 267, 223, 275, 236, 253, 246" href="/locations/illustration-studies-building/" alt="Illustration Studies Building"  >
+           <area shape="poly" coords="328, 56, 367, 50, 368, 76, 336, 80" href="/locations/41-meeting-street-studios/" alt="41 Meeting St."  >
+           <area shape="poly" coords="279, 17, 305, 13, 312, 45, 282, 47" href="/locations/what-cheer-studios/" alt="What Cheer Studios"  >
 		</map>
 	<img src="<?php echo get_template_directory_uri(); ?>/images/map5.png" width="613" height="757" usemap="#maploc">
 	<footer>
